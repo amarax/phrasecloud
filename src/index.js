dd1908dd47c68d2171423ca76b9a52d871fcec4c
Yong Lin <yonglin@me.com>
2024-01-10 19:21:22 +0800

    Working phrase cloud display with hover

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-
+import cloud from 'd3-cloud';
 
 const app = document.getElementById('app');
 
@@ -13,7 +13,65 @@ function onWorkerMessage(e) {
     console.log(msg);
     switch(msg.type) {
         case 'update':
-            updateStatus(`Found ${msg.content.sentences} sentences`);
+            updateStatus(`Found ${Object.entries(msg.content).map(([k,v])=>`${k}: ${v}`).join(', ')}`);
+            break;
+        case 'ngrams':
+            let ngrams = msg.content.ngrams;
+            let ngramList = Object.entries(ngrams).filter(([k,v])=>v.length>=5).map(([k,v])=>({ngram:k, responses:v})).sort((a,b)=>b.responses.length-a.responses.length);
+            
+            function draw(words) {
+                // Clear the svg
+                d3.select("#cloud").select('svg').remove();
+
+                d3.select("#cloud").append("svg")
+                    .attr("width", layout.size()[0])
+                    .attr("height", layout.size()[1])
+                  .append("g")
+                    .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+                  .selectAll("text")
+                    .data(words)
+                  .enter().append("text")
+                    .style("font-size", function(d) { return d.size + "px"; })
+                    .style("font-family", "Impact")
+                    .attr("text-anchor", "middle")
+                    .attr("transform", function(d) {
+                        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+                    })
+                    .text(function(d) { return d.text; })
+
+                    // On hover, show the responses
+                    .on('mouseover', function(d) {
+                        // Get responses for this ngram
+                        let responses = ngrams[d.target.innerHTML]?.map(r=>`<li>${r}</li>`).join('');
+                        d3.select('#responses').html(`<ul>${responses}</ul>`);
+                        // Remove the hidden class 
+                        // and position the top and left of the responses box to the position of the hovered text
+
+                        let rect = d3.select(this).node().getBoundingClientRect();
+                        let topleft = [rect.x + rect.width, rect.y]
+                        d3.select('#responses').classed('hidden', false)
+                            .style('top', `${topleft[1]}px`)
+                            .style('left', `${topleft[0]}px`);
+   
+                    })
+                    .on('mouseout', function(d) {
+                        d3.select('#responses').classed('hidden', true);
+                    });
+            }
+
+            let layout = cloud()
+                .size([512, 512])
+                .words(ngramList.map(function(d) {
+                    return {text: d.ngram, size: d.responses.length};
+                }))
+                .padding(5)
+                .font("Impact")
+                .rotate(()=>0)
+                .fontSize(function(d) { return d.size; })
+                .on("end", draw);
+        
+            layout.start();
+
             break;
         default:
             console.log('Unknown message type', msg.type);
@@ -25,16 +83,6 @@ function onWorkerMessage(e) {
 let worker = new Worker(new URL('./worker.js', import.meta.url));
 worker.onmessage = onWorkerMessage;
 if (module.hot) {
-    // module.hot.dispose(() => {
-    //     worker.terminate();
-    // });
-
-    // module.hot.accept('./worker.js', () => {
-    //     worker = new Worker(new URL('./worker.js', import.meta.url));
-    //     worker.onmessage = onWorkerMessage;
-    //     console.log(`Worker reloaded`);
-    // });
-
     // Force the worker to reload regardless
     module.hot.addStatusHandler((status) => {
         switch(status) {
@@ -52,10 +100,11 @@ if (module.hot) {
 
 const reader = new FileReader();
 reader.onload = (e) => {
-    const text = e.target.result;
+    let csv = d3.csvParse( e.target.result );
+    let responses = csv.map(row=>row['Responses']);
 
     // Send the text to a service worker to process
-    worker.postMessage({ text });
+    worker.postMessage({ responses });
 };
 
 let file = null;
@@ -75,5 +124,6 @@ fileInput.onchange = (e) => {
 document.getElementById('generate').onclick = (e) => {
     if(file) {
         reader.readAsText(file);
+
     }
 }
--- src/worker.js
+++ src/worker.js
@@ -10,18 +10,61 @@ const as = nlp.as;
 
 
 self.onmessage = (e) => {
-    const doc = nlp.readDoc(e.data.text);
+    const responses = e.data.responses.map(r=>nlp.readDoc(r).sentences());
     
-    const sentences = doc.sentences();
 
-    self.postMessage({type:'update', content:{sentences:sentences.length()}});
+    self.postMessage({type:'update', content:{responses:responses.length}});
 
-    const tokens = sentences.each(sentence => {
+    let tokenizedResponses = responses.map(sentences => {
+        
         // Remove punctuation and stop words
-        let tokens = sentence.tokens().filter( token => {
-            return token.out(its.type) === 'word' && token.out(its.stopWordFlag) === false;
-        } );
+        let tokens = [];
+        
+        sentences.each(sentence=>{
+            tokens.push( sentence.tokens().filter( token => {
+                return token.out(its.type) === 'word' && !token.out(its.stopWordFlag);
+            }) );
+        });
 
-        // console.log( tokens.out(its.lemma) );
+        return tokens;
     });
+
+    // Identify common ngrams, starting from token length 5 down to 1
+    let ngrams = {};
+    for(let i=5; i>0; i--) {
+        tokenizedResponses.forEach((tokens, responseIndex)=>{
+            tokens.forEach((t,sentenceIndex)=>{
+                let normalizedSentence = t.out(its.normal);
+
+                // Get all ngrams of length i
+                for(let j=0; j<normalizedSentence.length-i; j++) {
+                    let ngram = normalizedSentence.slice(j,j+i).join(' ');
+
+                    // If the ngram is a subset of another existing ngram, check if this instance is indeed the superset ngram
+                    // If so, skip this ngram
+                    // let superset = Object.keys(ngrams).find(k=>k.includes(ngram));
+                    // if(superset) {
+                    // // if(superset && normalizedSentence.join(' ').includes(superset)) {
+                    //     continue;
+                    // }
+
+                    if(ngrams[ngram]) {
+                        ngrams[ngram].push( e.data.responses[responseIndex] );
+                    } else {
+                        ngrams[ngram] = [ e.data.responses[responseIndex] ];
+                    }
+                }
+            });
+        });
+
+        // Remove all ngrams that only occur once
+        Object.entries(ngrams).forEach(([k,v])=>{
+            if(v===1) {
+                delete ngrams[k];
+            }
+        });
+
+        // Send ngrams to the main thread
+        self.postMessage({type:'ngrams', content:{ngrams}});
+    }
 };
