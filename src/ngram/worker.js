3ad4cc3924272bea4115f72bc74b906a4813c15b
Yong Lin <yonglin@me.com>
2024-04-01 03:44:01 +0800

    Working manual phrase structure

--- src/ngram/worker.js
+++ src/ngram/worker.js
@@ -59,11 +59,75 @@ function determineCategories(data) {
     return categories;
 }
 
+/**
+ * 
+ * @param {string} structureToken 
+ * @param {string} token 
+ * @returns {{match:boolean|null, remain?:boolean}}
+ */
+function isStructureMatch(token, structureToken, prevStructureToken) {
+    const Wildcard = '*';
+
+    if(structureToken instanceof Array) {
+        let m = false;
+        let r;
+        for(let s of structureToken) {
+            let {match, remain} = isStructureMatch(token, s, prevStructureToken);
+            if(match === true) {
+                return {match, remain};
+            } else if(match === null) {
+                m = null;
+                r = remain;
+            }
+        }
+        return {match:m, remain:r};
+    }
+
+    if(token.out(its.pos) == 'PART' || token.out(its.pos) == 'PUNCT') {
+        return {match:null, remain: true};
+    }
+
+    if(structureToken == Wildcard) {
+        if(token.out(its.stopWordFlag)) {
+            return {match:null, remain:true};
+        }
+
+        return {match:true};
+    }
+
+    if(structureToken === null)
+        return {match:null};
+
+    if(PartsOfSpeechTags.includes(structureToken)) {
+        if(token.out(its.pos) == structureToken) {
+            return {match:true};
+        }
+    }
+    if(structureToken == token.out()) {
+        return {match:true};
+    }
+
+    if(prevStructureToken == Wildcard) {
+        return {match:true, remain:true};
+    }
+    return {match:false};
+}
+
 function ngramsByStructure(dataset, structure, responseInclude) {
-    const patterns = [
-        { name: 'phrase-structure', patterns: [structure] }
-    ]
-    nlp.learnCustomEntities(patterns);
+    // const patterns = [
+    //     { name: 'phrase-structure', patterns: [structure] }
+    // ]
+    // nlp.learnCustomEntities(patterns);
+
+    let _structure = structure.split(' ').filter(s=>s.trim() != '');
+    _structure = _structure.map(s=>{
+        // If we start and end with square brackets, we split by the pipe
+        if(s.startsWith('[') && s.endsWith(']')) {
+            return s.substring(1, s.length - 1).split('|').map(s=>s.length > 0 ? s : null);
+        } else {
+            return [s];
+        }
+    });
 
     let responses = dataset
         .map(r=>nlp.readDoc(r.trim()));
@@ -75,59 +139,147 @@ function ngramsByStructure(dataset, structure, responseInclude) {
         });
     }
 
-    /** @type {Record<string, {responseIndex: number, match: any}>} */
+    /** @type {Record<string, {responseIndex: number, match: [number, number]}>} */
     let matches = {};
 
+    // Built-in matching, which doesn't work well with wildcards
+    // responses.forEach((response, responseIndex) => {
+    //     let _matches = response.customEntities();
+    //     _matches.each(match=>{
+    //         let key = match.out(its.stem).toLowerCase();
+    //         if(!matches[key]) {
+    //             matches[key] = [];
+    //         }
+    //         matches[key].push({responseIndex, match});
+    //     });
+    // });
+
     responses.forEach((response, responseIndex) => {
-        let _matches = response.customEntities();
-        _matches.each(match=>{
-            let key = match.out(its.stem).toLowerCase();
-            if(!matches[key]) {
-                matches[key] = [];
+        // Look for a match in the response
+        let tokens = response.tokens();
+
+        /** @type {{match:[number, number?], structureNextIndex: number, key:string[]}[]} */
+        let openMatches = [];
+
+        for(let i = 0; i < tokens.length(); i++) {
+            let token = tokens.itemAt(i);
+            
+            // First process open matches
+            for(let m of openMatches) {
+                let structureToken = _structure[m.structureNextIndex];
+                let prevStructureToken = m.structureNextIndex > 0 ? _structure[m.structureNextIndex - 1] : null;
+                let {match, remain} = isStructureMatch(token, structureToken, prevStructureToken);
+
+                if(match === true || match === null) {
+                    if(match === true)
+                        m.key.push(token.out(its.stem));
+
+                    if(!remain)
+                        m.structureNextIndex++;
+
+                    if(_structure[m.structureNextIndex] === undefined)
+                        // debugger;
+
+                    if(m.structureNextIndex == _structure.length) {
+                        // We have a match
+                        let k = m.key.join(' ');
+                        if(!matches[k]) {
+                            matches[k] = [];
+                        }
+                        matches[k].push({responseIndex, match:[m.match[0], i]});
+
+                        // Close the match if we don't end on a wildcard
+                        if((structureToken || prevStructureToken)[0] != '*') {
+                            openMatches = openMatches.filter(om=>om != m);
+                        }
+                    }
+                } else if(match === false) {
+                    openMatches = openMatches.filter(om=>om != m);
+                }
             }
-            matches[key].push({responseIndex, match});
-        });
+
+            // Then process the current token
+            let structureToken = _structure[0];
+            let {match, remain} = isStructureMatch(token, structureToken, null);
+            if(match === true) {
+                let openMatch = {match:[i], structureNextIndex: remain ? 0 : 1, key:[token.out(its.stem)]};
+                if(_structure.length == 1 && openMatch.structureNextIndex == 1) {
+                    // We have a single word match
+                    let k = openMatch.key.join(' ');
+                    if(!matches[k]) {
+                        matches[k] = [];
+                    }
+                    matches[k].push({responseIndex, match:[i,i]});
+                } else {
+                    openMatches.push(openMatch);
+                }
+            } else if(match === null) {
+                // openMatches.push({match:[i], structureNextIndex: remain ? 0 : 1, key:[]});
+            }
+        }
     });
 
-    // Remove ngrams that do not repeat
+    // Remove overlapping ngrams by favouring the longer ones
+    Object.entries(matches).forEach(([k,v])=>{
+        let toRemove = [];
+        v.forEach(({responseIndex:i1, match:m1}, i)=>{
+            v.forEach(({responseIndex:i2, match:m2}, j)=>{
+                if(i == j) return;
+                if(i1 != i2) return;
+                if(m1[0] >= m2[0] && m1[1] <= m2[1]) {
+                    toRemove.push(i);
+                }
+            });
+        });
+
+        matches[k] = v.filter((_,i)=>!toRemove.includes(i));
+    });
+
+    // Remove ngrams that do not repeat first, so the next subset checking is done much faster
     Object.entries(matches).forEach(([k,v])=>{
         if(v.length <= 1) {
             delete matches[k];
         }
     });
 
-    // Markup the matches
-    Object.values(matches).forEach(v=>{
-        v.forEach(({match})=>{
-            match.markup(MatchTags[0],MatchTags[1]);
-        });
-    });
-
-    let commonPhrases = {};
-    Object.entries(matches).forEach(([k,v])=>{
-        let phrases = {};
-        v.forEach(({match})=>{
-            let phrase = match.out();
-            if(!phrases[phrase]) {
-                phrases[phrase] = 0;
+    // If keys are a subset of another key, remove overlapping results
+    Object.entries(matches).forEach(([k1,v1])=>{
+        Object.entries(matches).forEach(([k2,v2])=>{
+            if(k1 == k2) return;
+            if(k2.includes(k1)) {
+                matches[k1] = v1.filter(({responseIndex:i1, match:m1})=>{
+                    return !v2.some(({responseIndex:i2, match:m2})=>{
+                        return i1 == i2 && m1[0] >= m2[0] && m1[1] <= m2[1];
+                    });
+                })
             }
-            phrases[phrase]++;
         });
-
-        let commonPhrase = Object.entries(phrases).sort((a,b)=>b[1]-a[1])[0][0];
-        commonPhrases[k] = commonPhrase;
     });
+
+    Object.entries(matches).forEach(([k,v])=>{
+        if(v.length <= 1) {
+            delete matches[k];
+        }
+    });
+
 
     let ngrams = {};
     Object.entries(matches).forEach(([k,v])=>{
+        let documentMap = new Map();
+        v.forEach(({responseIndex, match})=>{
+            let parent = responses[responseIndex];
+            if(!documentMap.has(parent)) {
+                documentMap.set(parent, nlp.readDoc(parent.out()));
+            }
+        });
+
         ngrams[k] = {
-            commonPhrase: commonPhrases[k],
             responses: v.map(({responseIndex, match})=>{
-                let parent = match.parentDocument();
-                let doc = nlp.readDoc(parent.out());
+                let parent = responses[responseIndex];
+                let doc = documentMap.get(parent);
                 let tokens = doc.tokens();
 
-                let span = match.out(its.span);
+                let span = match;
 
                 let first = tokens.itemAt( span[0] );
                 let last = tokens.itemAt( span[1] );
@@ -145,8 +297,30 @@ function ngramsByStructure(dataset, structure, responseInclude) {
                 }
             })
         }
-    });
 
+        // Find the most common phrase by looking for regex matches in the duplicated documents
+        // Build the regex match from the tags
+        let regex = new RegExp(`${MatchTags[0]}([\\s\\S]*?)${MatchTags[1]}`, 'g');
+        let phrases = {}
+        documentMap.forEach(doc=>{
+            let matches = doc.out(its.markedUpText).match(regex);
+            if(matches) {
+                matches.forEach(m=>{
+                    let phrase = m.substring(MatchTags[0].length, m.length - MatchTags[1].length);
+                    if(!phrases[phrase]) {
+                        phrases[phrase] = 0;
+                    }
+                    phrases[phrase]++;
+                });
+            } else {
+                console.error('No matches', doc.out(its.markedUpText));
+            }
+        });
+
+        let commonPhrase = Object.entries(phrases).sort((a,b)=>b[1]-a[1])[0][0];
+        
+        ngrams[k].commonPhrase = commonPhrase;
+    });
 
     return ngrams;
 }
@@ -411,7 +585,7 @@ const PartsOfSpeechTags = [
     'PART',
     'PRON',
     'PROPN',
-    // 'PUNCT',
+    'PUNCT',
     'SCONJ',
     'SYM',
     'VERB',
@@ -449,7 +623,8 @@ self.onmessage = (e) => {
             const ANYOREMPTY = `[|${PartsOfSpeechTags.join('|')}]`;
             const ANYUpTo3Words = `${ANY}`;
 
-            phraseStructure = e.data.phraseStructure.replace(/\*/g, ANYUpTo3Words);
+            // phraseStructure = e.data.phraseStructure.replace(/\*/g, ANYUpTo3Words);
+            phraseStructure = e.data.phraseStructure;
         }
     }
 
